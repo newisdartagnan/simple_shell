014fd0115354d12a944877a91d8dc7ff56031c43
newisdartagnan <newisboris@gmail.com>
2022-11-17 01:38:18 -0800

    my merge

--- main.h
+++ main.h
@@ -18,20 +18,6 @@
 
 extern char **environ;
 
-char **tInterface(char *, const char *, int);
-int count_token(char *, const char *);
-char **tokenize(int, char *, const char *);
-void create_child(char **, char *, int, char **);
-void parse_line(char *, size_t, int, char **);
-char *path_finder(char *);
-int str_len(char *);
-int find_index(char *);
-char **tokenize_path(int, char *);
-char *search_directories(char **, char *);
-char *build_path(char *, char *);
-void double_free(char **);
-void single_free(int, ...);
-
 
 /* featured functions*/
 void error_printing(char *, int, char *);
